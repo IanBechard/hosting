f22e204a7bd3d996bbaff95acdd522832ee37725
IanBechard <43045250+IanBechard@users.noreply.github.com>
2022-09-21 15:59:40 -0400

    java init

---
+++ src/test/java/com/hosting/backend/HostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosting.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
